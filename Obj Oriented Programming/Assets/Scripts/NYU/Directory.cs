f092f85ddfeed27be51bd2ba59f6dc4b61dbd808
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-04-23 15:06:55 -0400

    deleted temp project added own project

---
+++ Obj Oriented Programming/Assets/Scripts/NYU/Directory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Directory : MonoBehaviour
+{
+    public Text text; //variable for component that displays the text
+
+    //NOTE: notice that this is a list of NYUPerson, but can still hold all subclasses. THAT'S POLYMORPHISM, BABY!
+    public List<NYUPerson> nyuPeopleList; //list of people at nyu
+
+    int recordNum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nyuPeopleList = new List<NYUPerson>();
+
+        //make a new NYUFaculty 
+        NYUPerson mattParker = new NYUFaculty("Matt Parker",
+            "mp612",
+            9999999.12f, false);
+
+        NYUPerson diane = new NYUStudent("Diane Wang",
+            "rw2324",
+            5.0f, 34);
+
+        NYUPerson andy = new NYUStaff("Andy Hamilton",
+            "ah0001", 2000000);
+        
+        nyuPeopleList.Add(mattParker); //add mattParker to the nyuPeopleList
+        nyuPeopleList.Add(diane);
+        nyuPeopleList.Add(andy);
+        
+        UpdateRecord(); //update the text display to show the the info from the first person in the list
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){ //if space is pressed
+            UpdateRecord(); //change the record to display the next person in the list
+        }
+    }
+
+    void UpdateRecord(){
+        NYUPerson person = nyuPeopleList[recordNum % nyuPeopleList.Count]; //grab the object a person in the list (use % to wrap around)
+
+        text.text = person.ShowRecord(); //display the info for this object
+
+        recordNum++; //increase the recordNum so we show the next person next time through
+    }
+}
